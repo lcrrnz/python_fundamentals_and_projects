9a7524dd2275064b7894f9caf9407447744a9bdf
Luis Carranza <ghojan_14@hotmail.com>
2025-05-19 17:51:39 -0600

    Updated calculate_average

--- semana_10_project/actions/calculate_average.py
+++ semana_10_project/actions/calculate_average.py
@@ -1,19 +1,25 @@
 def scores_average(students_info):
     try:
-        averages = {} 
         if not students_info:
             print("No student data available. Please enter students first")
-        else:
-            print("Calculating students' averages. Please wait...")
-            for student in students_info:  
-                name = student.get("name", "Unknown")  
-                scores = [int (value) for key, value in student.items() if key.endswith("score")]
-                if not scores: 
-                    print(f"Unable to calculate average for {name} (No scores provided)")
-                else:
-                    averages[name] = sum(scores) / len(scores)
-            print(averages)
-            
+            return {}
+        print("Calculating scores averages. Please wait...")
+        subject_scores = {
+            "spanish score": [],
+            "english score": [],
+            "socials score": [],
+            "science score": []
+        }
+        for student in students_info:
+            for key, value in student.items():
+                if key in subject_scores:  
+                    subject_scores[key].append(int(value))  
+        averages = {}
+        for subject, scores in subject_scores.items():
+            if scores:  
+                averages[subject] = sum(scores) / len(scores)
+        print(f'Average scores per subject:\n {averages}')
+        return averages
     except Exception as error:
         print(f"Unable to complete due to {error}")
-    return averages
+        return {}
